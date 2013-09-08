4b5c0ed30fd9c002d51a7fc5e18ddb258be27e66
Tahsin Hasan <tahsin@grameentek.com>
2013-09-08 15:23:50 +0600

    s

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart156/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart156/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart156/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart156/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart156/admin/');
+define('DIR_SYSTEM', '/var/www/opencart156/system/');
+define('DIR_DATABASE', '/var/www/opencart156/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencart156/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencart156/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencart156/system/config/');
+define('DIR_IMAGE', '/var/www/opencart156/image/');
+define('DIR_CACHE', '/var/www/opencart156/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart156/download/');
+define('DIR_LOGS', '/var/www/opencart156/system/logs/');
+define('DIR_CATALOG', '/var/www/opencart156/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart156');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart156/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart156/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart156/catalog/');
+define('DIR_SYSTEM', '/var/www/opencart156/system/');
+define('DIR_DATABASE', '/var/www/opencart156/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencart156/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/opencart156/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/opencart156/system/config/');
+define('DIR_IMAGE', '/var/www/opencart156/image/');
+define('DIR_CACHE', '/var/www/opencart156/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart156/download/');
+define('DIR_LOGS', '/var/www/opencart156/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart156');
+define('DB_PREFIX', 'oc_');
+?>
